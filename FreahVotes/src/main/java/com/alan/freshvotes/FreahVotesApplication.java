95d0ca7c4a84d9ef062b1f16a427008481336ffa
Rooney, Alan <Alan.Rooney@ca.com>
2018-09-18 22:53:07 +0100

    Base boot.io image

---
+++ FreahVotes/src/main/java/com/alan/freshvotes/FreahVotesApplication.java
@@ -0,0 +1,12 @@
+package com.alan.freshvotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreahVotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreahVotesApplication.class, args);
+	}
+}
