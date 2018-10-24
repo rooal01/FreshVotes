c670aead4d2ccd581a58015722399c6aee3b997a
Rooney, Alan <Alan.Rooney@ca.com>
2018-10-24 20:06:46 +0100

    Step one : Added database users authentication

---
+++ FreahVotes/src/main/java/com/alan/freshvotes/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.alan.freshvotes.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alan.freshvotes.domain.User;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+//Note in above we have provided the domain object User and its ID type 
+	
+//	This works becuase we have a Username field in the object user. Important to use findByproperty convention...
+	User findByUsername(String username);
+
+}
---
+++ FreahVotes/src/main/java/com/alan/freshvotes/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.alan.freshvotes.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.alan.freshvotes.domain.User;
+import com.alan.freshvotes.repositories.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+//	Note how we implement the UserDetailsService interface and the naming convention used on our interface. This is standard practice
+
+	@Autowired
+	private UserRepository userRepo; //Note how the simple addition of this repository provides us will all CRUD operations 
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		
+		User user = userRepo.findByUsername(username);
+		
+		if (user == null)
+			throw new UsernameNotFoundException("Invalid Username and Password");
+		return null;
+	}
+
+}
