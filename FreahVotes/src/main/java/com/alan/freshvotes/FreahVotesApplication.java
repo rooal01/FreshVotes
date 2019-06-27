6507048de5dc2ec498bf2f74b158ab8f636c93df
Rooney, Alan <Alan.Rooney@ca.com>
2019-06-27 10:40:54 +0100

    added . infront of all hrefs (./login)

--- FreahVotes/src/main/java/com/alan/freshvotes/FreahVotesApplication.java
+++ FreahVotes/src/main/java/com/alan/freshvotes/FreahVotesApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FreahVotesApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("server.servlet.context-path", "/alansfreshvotes");
 		SpringApplication.run(FreahVotesApplication.class, args);
 	}
 }
