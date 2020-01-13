6c1018f6f83e2a652f4a14c0ede3792dbd28bd1a
Rooney, Alan <Alan.Rooney@ca.com>
2020-01-13 12:17:02 +0000

    Comments on how foreign keys are created.

--- FreahVotes/src/main/java/com/alan/freshvotes/domain/VoteId.java
+++ FreahVotes/src/main/java/com/alan/freshvotes/domain/VoteId.java
@@ -6,6 +6,10 @@ import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
+//This creates the required foreign key relationship between the vote table the user and feature tables
+//for the features the feature_id in the votes table gets mapped to the id in the features table
+//for the users the user_id in the votes table gets mapped to the id in the users table.
+//the user_id and feature_id entries are automatically created.
 @Embeddable
 public class VoteId implements Serializable{
 	
